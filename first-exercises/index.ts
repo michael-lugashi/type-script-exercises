05a97dd5d91402880032355f32336bf63aef8bb1
michael-lugashi <michael.lugashi@gmail.com>
2021-12-10 22:32:32 +0200

    added post request to calculator

--- first-exercises/index.ts
+++ first-exercises/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import calculateBmi from './bmiCalc';
+import calculator from './calculator';
 const app = express();
 const PORT = 3003;
 
@@ -10,21 +11,31 @@ app.get('/hello', (_req, res) => {
  res.send('Hello Full Stack!');
 });
 app.get('/bmi', (req, res) => {
+ const { height, weight } = req.query;
  try {
+  if (isNaN(Number(height) + Number(weight))) {
+   throw new Error('malformatted parameters');
+  }
   interface response {
    weight: number;
    height: number;
    bmi: string;
   }
   const response: response = {
-   weight: Number(req.query.weight),
-   height: Number(req.query.height),
-   bmi: calculateBmi(Number(req.query.height), Number(req.query.weight)),
+   weight: Number(height),
+   height: Number(weight),
+   bmi: calculateBmi(Number(height), Number(weight)),
   };
   res.json(response);
  } catch (error) {
-  res.status(400).json({ error: 'malformatted parameters' });
+  res.status(400).json({ error: error.message });
  }
+});
+
+app.post('/calculate', (req, res) => {
+ const { value1, value2, op } = req.body;
+ const result = calculator(value1, value2, op);
+ res.send(result);
 });
 
 app.listen(PORT, () => {
