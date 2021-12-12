86964e2dc5cb806850d2689145ed0b961d5268f3
michael-lugashi <michael.lugashi@gmail.com>
2021-12-12 23:18:03 +0200

    set up server for second project

--- second-exercises/index.ts
+++ second-exercises/index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 8080;
+const options: cors.CorsOptions = {
+  origin: '*'
+};
 
-app.get('/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
+app.use(cors(options));
+
+app.get('/api/ping', (_req, res) => {
+ console.log('someone pinged here');
+ res.send('pong');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+ console.log(`Server running on port ${PORT}`);
 });
